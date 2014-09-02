8147bbdba0279000d4534ca2bda3d8e5b552a9aa
Lee Briggs <lee.briggs@headlondon.com>
2014-09-02 11:17:07 +0100

    +	service is now passed the ENV constant to resolve endpoints

--- app/scripts/services/patientSearch.js
+++ app/scripts/services/patientSearch.js
@@ -8,21 +8,23 @@
  * Controller of the openeyesApp
  */
 angular.module('openeyesApp')
-  .factory('patientSearch', ['$http', function($http) {
+  .factory('patientSearch', ['$http', 'ENV', function($http, ENV) {
 
     return {
       findPatients: function(searchTerm){
         console.log('searching...', searchTerm);
+        console.log(ENV);
         return $http({
           method: 'GET',
-          url: 'http://localhost:9000/data/patients.json'
+          url: ENV.host + ENV.apiEndpoints.patients
         });
       },
       getPatient: function(id){
         console.log('retrieving...', id);
+        var apiCall = (ENV.name === 'dev') ? ENV.host + ENV.apiEndpoints.patient.replace('<id>', id) : ENV.apiEndpoints.patient + id;
         return $http({
           method: 'GET',
-          url: 'http://localhost:9000/data/patient-' + id + '.json'
+          url: apiCall
         });
       }
     };
