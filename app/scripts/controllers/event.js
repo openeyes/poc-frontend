bd1450f4b3cfa0618f8a5b375425749d441e3c95
Lee Briggs <lee.briggs@headlondon.com>
2014-09-02 11:53:50 +0100

    +	stub event controller and view

---
+++ app/scripts/controllers/event.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name openeyesApp.controller:EventCtrl
+ * @description
+ * # EventCtrl
+ * Controller of the openeyesApp
+ */
+angular.module('openeyesApp')
+  .controller('EventCtrl', function ($scope) {
+		console.log($scope);
+  });
