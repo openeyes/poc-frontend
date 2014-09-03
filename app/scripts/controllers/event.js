7648f249c508f9d0abf2b77f929cb29180e78c8d
Lee Briggs <lee.briggs@headlondon.com>
2014-09-03 14:27:05 +0100

    +	added event loading to event controller

--- app/scripts/controllers/event.js
+++ app/scripts/controllers/event.js
@@ -8,7 +8,24 @@
  * Controller of the openeyesApp
  */
 angular.module('openeyesApp')
-	.controller('EventCtrl', function ($scope, $window) {
+	.controller('EventCtrl', function ($scope, $window, $routeParams, Event) {
+
+		$scope.event = null;
+
+		function loadEvent(patientId, eventId){
+			console.log('loading event', eventId);
+			Event.getEvent(patientId, eventId)
+				.success(function(data) {
+					$scope.event = data;
+	      })
+	      .error(function(data, status, headers, config) {
+					console.log(data, status, headers, config);
+		    });
+		}
+
+		if($routeParams.patientId && $routeParams.eventId){
+			loadEvent($routeParams.patientId, $routeParams.eventId);
+		}
 
 		var eyedrawOptions = {
 			doodles: [
