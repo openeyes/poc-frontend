4dc219ad4631e8a67e011e748584764df0425341
Lee Briggs <lee.briggs@headlondon.com>
2014-09-03 14:26:43 +0100

    +	config updated with event endpoints

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'dev',host:'http://localhost:9000',apiEndpoints:{patients:'/data/patients.json',patient:'/data/patient-<id>.json',sites:'/data/sites.json',siteLasers:'/data/lasers-site-<id>.json',laserOperators:'/data/laser-operators.json'}})
+.constant('ENV', {name:'dev',host:'http://localhost:9000',apiEndpoints:{patients:'/data/patients.json',patient:'/data/patient-<id>.json',sites:'/data/sites.json',siteLasers:'/data/lasers-site-<id>.json',laserOperators:'/data/laser-operators.json',patientEvents:'/data/patient-<id>-events.json',event:'/data/patient-<pid>-event-<eid>.json'}})
 
 ;
