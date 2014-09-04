4839fc6645a9a472c02b5468889a8c90254cab53
Lee Briggs <lee.briggs@headlondon.com>
2014-09-04 11:02:23 +0100

    +	switched to new endpoint structure

--- app/scripts/services/patientSearch.js
+++ app/scripts/services/patientSearch.js
@@ -12,17 +12,16 @@ angular.module('openeyesApp')
 
     return {
       findPatients: function(searchTerm){
-        console.log(searchTerm);
-        // var apiCall = (ENV.name === 'dev') ? ENV.host + ENV.apiEndpoints.patients : ENV.apiEndpoints.patients + '=' + searchTerm;
-        var apiCall = ENV.host + ENV.apiEndpoints.patients;
+        searchTerm = encodeURIComponent(searchTerm);
+        var apiCall = ENV.host + ENV.apiEndpoints.patients.replace(':term', searchTerm);
+        console.log(apiCall);
         return $http({
           method: 'GET',
           url: apiCall
         });
       },
       getPatient: function(id){
-        // var apiCall = (ENV.name === 'dev') ? ENV.host + ENV.apiEndpoints.patient.replace('<id>', id) : ENV.apiEndpoints.patient + id;
-        var apiCall = ENV.host + ENV.apiEndpoints.patient.replace('<id>', id);
+        var apiCall = ENV.host + ENV.apiEndpoints.patient.replace(':id', id);
         return $http({
           method: 'GET',
           url: apiCall
