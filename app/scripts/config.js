cbfb036dd64ec6b7a4ea06c4009d72afc0dc5199
Lee Briggs <lee.briggs@headlondon.com>
2014-09-08 09:50:43 +0100

    +	added custom scripts to allow npm run deploy +	also added bower as local dependency rather than global

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -2,6 +2,6 @@
 
  angular.module('config', [])
 
-.constant('ENV', {name:'dev',host:'http://localhost:8080',apiEndpoints:{patients:'/Patient/?searchTerm=:term',patient:'/Patient/:id',sites:'/Site',siteLasers:'/Laser/?siteId=:id',laserOperators:'/LaserOperator',patientEvents:'/LaserEvent/?patientId=:id',laserEvent:'/LaserEvent/:id',createLaserEvent:'/LaserEvent',procedures:'/Procedure'}})
+.constant('ENV', {name:'dist',host:'http://openeyes:Aeno8aiKu0ih@openeyes-api-uat.headlondon.com',apiEndpoints:{patients:'/Patient/?searchTerm=:term',patient:'/Patient/:id',sites:'/Site',siteLasers:'/Laser/?siteId=:id',laserOperators:'/LaserOperator',patientEvents:'/LaserEvent/?patientId=:id',laserEvent:'/LaserEvent/:id',createLaserEvent:'/LaserEvent',procedures:'/Procedure'}})
 
 ;
