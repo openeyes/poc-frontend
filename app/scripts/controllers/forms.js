15e9361c02b77aeb5f1a6c56bb0fd60368d3283c
Richard <willis.rh@gmail.com>
2014-09-09 23:22:29 +0100

    Moved validation logic into directives

--- app/scripts/controllers/forms.js
+++ app/scripts/controllers/forms.js
@@ -8,48 +8,7 @@
  * Controller of the openeyesApp
  */
 angular.module('openeyesApp')
-	.directive('validate', ['$compile',function($compile) {
-		return {
-			restrict: 'A',
-			replace: false,
-			// Skip other directives with lower priority (we'll be using $compile to compile other directives)
-			terminal: true,
-			// Execute before other directives
-			priority: 1000,
-			link: function(scope, element, attrs) {
 
-				var rules = scope.$eval(attrs.validate);
-
-				Object.keys(rules).forEach(function(rule) {
-					var val = rules[rule];
-					switch(rule) {
-						case 'required':
-							element.attr('required', val);
-						break;
-						case 'pattern':
-							element.attr('ng-pattern', val);
-						break;
-						case 'minlength':
-							element.attr('ng-minlength', val);
-						break;
-						case 'maxlength':
-							element.attr('ng-maxlength', val);
-						break;
-						case 'min':
-							element.attr('min', val);
-						break;
-						case 'max':
-							element.attr('max', val);
-						break;
-					}
-				});
-
-				element.removeAttr('validate');
-
-				$compile(element)(scope);
-			}
-		};
-	}])
 	.controller('FormsCtrl', ['$scope', '$window', function ($scope, $window) {
 
 		$scope.submitted = false;
@@ -69,8 +28,14 @@ angular.module('openeyesApp')
 			name: {
 				minlength: 2,
 				maxlength: 5,
-				required: true,
-				pattern: '/^[^0-9]+$/'
+				required: {
+					value: true,
+					msg: 'The name field is definately required'
+				},
+				pattern: {
+					value: '/^[^0-9]+$/',
+					msg: 'No numbers!'
+				}
 			},
 			location: {
 				required: true
@@ -84,23 +49,6 @@ angular.module('openeyesApp')
 		};
 
 		$scope.formName = 'form';
-
-		// Has the field value changed, or has the form attempted to be submitted?
-		function isDirty(field) {
-			return ($scope.submitted || field.$dirty);
-		}
-
-		// Check if a field has any errors
-		$scope.hasFieldError = function(field, rule) {
-
-			if (typeof field === 'string') {
-				field = $scope[$scope.formName][field];
-			}
-
-			var isInvalid = (isDirty(field) && field.$invalid);
-
-			return isInvalid && (rule ? field.$error[rule] : true);
-		};
 
 		// Form submit handler
 		$scope.submit = function() {
---
+++ app/scripts/directives/validation.js
@@ -0,0 +1,121 @@
+(function() {
+
+	'use strict';
+
+	function isDirty($scope, field) {
+		return ($scope.submitted || field.$dirty);
+	}
+
+	function hasError($scope, name, rule) {
+
+		var field = $scope[$scope.formName][name];
+		var hasError = (isDirty($scope, field) && field.$invalid);
+		var hasErrorForRule = hasError && (rule ? field.$error[rule] : true);
+
+		return hasErrorForRule;
+	}
+
+	var ValidationCtrl = function(attrName) {
+		return [
+			'$scope','$element','$attrs',
+			function ValidationCtrl($scope, $element, $attrs) {
+
+				$scope.hasError = function(rule) {
+					return hasError($scope, $attrs[attrName], rule);
+				};
+
+				// Check if an object has any true values (used in checkbox groups)
+				$scope.hasObjectSelection = function(model, fieldName) {
+					var field = $scope[$scope.formName][fieldName];
+					return isDirty(field) && Object.keys(model).some(function(key) {
+						return (model[key] === true);
+					});
+				};
+			}
+		];
+	};
+
+	angular.module('openeyesApp')
+		.directive('oeValidate', [function() {
+			return {
+				restrict: 'A',
+				require: '^form',
+				scope: true,
+				link: function(scope, element, attr) {
+
+					var fieldName = attr.oeValidate;
+					var className = attr.oeValidateClassName || 'has-error';
+
+					if (!fieldName) {
+						throw new Error('oeValidate: field name not specified.')
+					}
+
+					scope.$watchCollection('[' + [
+						'form.' + fieldName + '.$valid',
+						'form.' + fieldName + '.$dirty',
+						'submitted'
+					].join(',') + ']', function updateClass() {
+						if (hasError(scope, fieldName)) {
+							element.addClass(className);
+						} else {
+							element.removeClass(className);
+						}
+					});
+				}
+			};
+		}])
+		.directive('oeValidateMsg', [function() {
+			return {
+				restrict: 'E',
+				replace: true,
+				scope: true,
+				templateUrl: 'views/directives/fieldValidationMsg.html',
+				controller: ValidationCtrl('name')
+			}
+		}])
+		.directive('oeValidateRules', ['$compile',function($compile) {
+			function getVal(val) {
+				return angular.isObject(val) ? val.value : val;
+			}
+			return {
+				restrict: 'A',
+				replace: false,
+				// Skip other directives with lower priority (we'll be using $compile to compile other directives)
+				terminal: true,
+				// Execute before other directives
+				priority: 1000,
+				link: function(scope, element, attrs) {
+
+					var rules = scope.$eval(attrs['oeValidateRules']);
+
+					Object.keys(rules).forEach(function(rule) {
+						var val = rules[rule];
+						switch(rule) {
+							case 'required':
+								element.attr('required', getVal(val));
+							break;
+							case 'pattern':
+								element.attr('ng-pattern', getVal(val));
+							break;
+							case 'minlength':
+								element.attr('ng-minlength', getVal(val));
+							break;
+							case 'maxlength':
+								element.attr('ng-maxlength', getVal(val));
+							break;
+							case 'min':
+								element.attr('min', getVal(val));
+							break;
+							case 'max':
+								element.attr('max', getVal(val));
+							break;
+						}
+					});
+
+					element.removeAttr('oe-validate-rules');
+
+					$compile(element)(scope);
+				}
+			};
+		}]);
+}());
