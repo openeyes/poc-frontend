a42ddf8e945db9f385e2ea835d30b0887cb51755
Lee Briggs <lee.briggs@headlondon.com>
2014-09-12 12:22:16 +0100

    -	removed build of eyedraw to straight copy task

--- Gruntfile.js
+++ Gruntfile.js
@@ -359,7 +359,8 @@ module.exports = function (grunt) {
 						'*.html',
 						'views/{,*/}*.html',
 						'images/{,*/}*.{webp}',
-						'fonts/*'
+						'fonts/*',
+						'eyedraw/{,*/*}*'
 					]
 				}, {
 					expand: true,
