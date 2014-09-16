46a29196db9b834157726c9516c60dba161e36bf
Lee Briggs <lee.briggs@headlondon.com>
2014-09-16 14:03:33 +0100

    +	moved components into own view directory

--- app/scripts/services/event.js
+++ app/scripts/services/event.js
@@ -49,7 +49,8 @@ angular.module('openeyesApp')
             'laserSite',
             'procedures',
             'eyedraw',
-            'treatment'
+            'treatment',
+            'history'
           ],
           eventType2: [
             'procedures',
@@ -63,15 +64,16 @@ angular.module('openeyesApp')
       getComponentMappings: function(mode){
         var componentMappings = {
           edit: {
-            laserSite: '<section class="element panel panel-default"><div class="panel-heading"><h3 class="panel-title">Site</h3></div><div class="panel-body"><lasersite ng-model="laserDetails"></lasersite></div></section>',
-            procedures: '<section class="element panel panel-default"><div class="panel-heading"><h3 class="panel-title">Treatment</h3></div><div class="panel-body"><div class="row"><div class="col-xs-6"><div class="form-group"><label for="procedure-right" class="col-xs-4 control-label">Procedures:</label><div class="col-xs-8"><procedure-selection data-id="procedure-right" data-side="rightEye" ng-model="procedures"></procedure-selection></div></div></div><div class="col-xs-6"><div class="form-group"><label for="procedure-left" class="col-xs-4 control-label">Procedures:</label><div class="col-xs-8"><procedure-selection data-id="procedure-left" data-side="leftEye" ng-model="procedures"></procedure-selection></div></div></div></div></div></section>',
-            eyedraw: '<section class="element panel panel-default"><div class="panel-heading"><h3 class="panel-title">Anterior Segment</h3></div><div class="panel-body"><div class="row"><div class="col-xs-6"><eyedraw data="data" data-side="rightEye" options="anterior" mode="{{mode}}"></eyedraw></div><div class="col-xs-6"><eyedraw data="data" data-side="leftEye" options="anterior" mode="{{mode}}"></eyedraw></div></div></div></section>',
-            treatment: '<section class="element panel panel-default"><div class="panel-heading"><h3 class="panel-title">Treatment</h3></div><div class="panel-body"><div class="row"><div class="col-xs-6"><treatment data-side="rightEye"></treatment></div><div class="col-xs-6"><treatment data-side="leftEye"></treatment></div></div></div></section>'
+            laserSite: '<ng-include src="\'/views/components/edit/laserSite.html\'"></ng-include>',
+            procedures: '<ng-include src="\'/views/components/edit/procedures.html\'"></ng-include>',
+            eyedraw: '<ng-include src="\'/views/components/edit/eyedraw.html\'"></ng-include>',
+            treatment: '<ng-include src="\'/views/components/edit/treatment.html\'"></ng-include>',
+            history: '<ng-include src="\'/views/components/edit/history.html\'"></ng-include>'
           },
           view: {
-            laserSite: '<section class=""><div class="row"><div class="panel panel-default"><div class="panel-heading"><h3 class="panel-title">Site</h3></div><div class="panel-body"><div class="row"><div class="col-xs-2">Site:</div><div class="col-xs-4">{{ event.site.label }}</div></div><div class="row"><div class="col-xs-2">Laser:</div><div class="col-xs-4">{{ event.laser.label }}</div></div><div class="row"><div class="col-xs-2">Operater:</div><div class="col-xs-4">{{ event.laserOperator.firstName }} {{ event.laserOperator.surname }}</div></div></div></div></div></section>',
-            procedures: '<section class=""><div class="row"><div class="panel panel-default"><div class="panel-heading"><h3 class="panel-title">Treatment</h3></div><div class="panel-body"><div class="row"><div class="col-xs-6"><div class="row" ng-if="event.rightEye.procedures && event.rightEye.procedures.length"><div class="col-xs-4">Procedures:</div><div class="col-xs-8"><ul><li ng-repeat="procedure in event.rightEye.procedures">{{procedure.label}}</li></ul></div></div></div><div class="col-xs-6"><div class="row" ng-if="event.leftEye.procedures && event.leftEye.procedures.length"><div class="col-xs-4">Procedures:</div><div class="col-xs-8"><ul><li ng-repeat="procedure in event.leftEye.procedures">{{procedure.label}}</li></ul></div></div></div></div></div></div></div></section>',
-            eyedraw: '<section class=""><div class="row"><div class="panel panel-default"><div class="panel-heading"><h3 class="panel-title">Anterior Segment</h3></div><div class="panel-body"><div class="row"><div class="col-xs-6"><eyedraw data="{{ event.rightEye.anteriorSegment.data }}" options="anterior" mode="view"></eyedraw></div><div class="col-xs-6"><eyedraw data="{{ event.leftEye.anteriorSegment.data }}" options="anterior" mode="view"></eyedraw></div></div></div></div></div></section>'
+            laserSite: '<ng-include src="\'/views/components/view/laserSite.html\'"></ng-include>',
+            procedures: '<ng-include src="\'/views/components/view/procedures.html\'"></ng-include>',
+            eyedraw: '<ng-include src="\'/views/components/view/eyedraw.html\'"></ng-include>'
           }
           
         };
