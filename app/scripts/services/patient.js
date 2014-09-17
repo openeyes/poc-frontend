0dd5c9006bc188a10e2a1471d8b639a7652e14fc
Lee Briggs <lee.briggs@headlondon.com>
2014-09-17 16:33:46 +0100

    +	added fake patient data to service

--- app/scripts/services/patient.js
+++ app/scripts/services/patient.js
@@ -2,12 +2,30 @@
 
 /**
  * @ngdoc function
- * @name openeyesApp.service:patient
+ * @name openeyesApp.service:Patient
  * @description
- * # patientService
+ * # Patient
  * Controller of the openeyesApp
  */
 angular.module('openeyesApp')
-  .factory('Patient', ['$resource', function($resource) {
-    return $resource('/api/entries/:id'); // Note the full endpoint address
+  .factory('Patient', ['$http', '$q', 'ENV', function($http, $q, ENV) {
+
+    console.log(ENV);
+
+    return {
+
+      getExistingAllergies: function(patientId){
+        console.log(patientId);
+
+        var data = [
+          'Atropine',
+          'Fluorescin'
+        ];
+
+        var deferred = $q.defer();
+        deferred.resolve(data);
+
+        return deferred.promise;
+      }
+    };
   }]);
