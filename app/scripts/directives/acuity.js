1a7fbe2dfaba8f4e4a057591280bac58a0d8a80e
Lee Briggs <lee.briggs@headlondon.com>
2014-09-17 16:32:15 +0100

    +	switched tabs for spaces

--- app/scripts/directives/acuity.js
+++ app/scripts/directives/acuity.js
@@ -8,40 +8,40 @@
  * Controller of the openeyesApp
  */
 angular.module('openeyesApp')
-	.controller('AcuityCtrl', ['$scope', 'Acuity', 'Event', function($scope, Acuity, Event){
+  .controller('AcuityCtrl', ['$scope', 'Acuity', 'Event', function($scope, Acuity, Event){
 
-		var self = this;
+    var self = this;
 
-		this.init = function(attrs){
-			//	Listen for save event
-			//	Broadcast by event page controller
+    this.init = function(attrs){
+      //  Listen for save event
+      //  Broadcast by event page controller
       this.eyeSide = attrs.side;
-			$scope.model = {};
-			$scope.model.acuityMeasurements = [];
-			$scope.$on('event.save', this.broadcastModel);
-			//	On creation populate dropdown
+      $scope.model = {};
+      $scope.model.acuityMeasurements = [];
+      $scope.$on('event.save', this.broadcastModel);
+      //  On creation populate dropdown
 
-			Acuity.getAcuityFields()
-				.then(function(data) {
-					$scope.measurements = data.measurements;
+      Acuity.getAcuityFields()
+        .then(function(data) {
+          $scope.measurements = data.measurements;
           $scope.corrections = data.corrections;
-				}, function(error) {
-					console.log(error);
-				});
-		};
+        }, function(error) {
+          console.log(error);
+        });
+    };
 
-		this.broadcastModel = function(){
-			Event.addToEventStack(self.getModel());
-		};
+    this.broadcastModel = function(){
+      Event.addToEventStack(self.getModel());
+    };
 
-		this.getModel = function(){
+    this.getModel = function(){
       console.log('get the model', $scope.model);
-			return {
-				name: 'acuity',
+      return {
+        name: 'acuity',
         subPath: this.eyeSide,
-				model: $scope.model
-			};
-		};
+        model: $scope.model
+      };
+    };
 
     // $scope methods
     $scope.addRow = function(){
@@ -55,22 +55,22 @@ angular.module('openeyesApp')
       $scope.model.acuityMeasurements.splice(index, 1);
     };
 
-	}])
-	/**
-	 * @ngdoc function
-	 * @name openeyesApp.directive:history
-	 * @description
-	 * # history
-	 * Directive of the openeyesApp
-	 */
-	.directive('acuity', [function () {
-		return {
-			restrict: 'EA', //E = element, A = attribute, C = class, M = comment
-			scope: {},
-			templateUrl: 'views/directives/acuity.html',
-			controller: 'AcuityCtrl', //Embed a custom controller in the directive
-			link: function (scope, element, attrs, AcuityCtrl) {
-				AcuityCtrl.init(attrs);
-			}
-		};
-	}]);
+  }])
+  /**
+   * @ngdoc function
+   * @name openeyesApp.directive:acuity
+   * @description
+   * # acuity
+   * Directive of the openeyesApp
+   */
+  .directive('oeAcuity', [function () {
+    return {
+      restrict: 'EA', //E = element, A = attribute, C = class, M = comment
+      scope: {},
+      templateUrl: 'views/directives/acuity.html',
+      controller: 'AcuityCtrl', //Embed a custom controller in the directive
+      link: function (scope, element, attrs, AcuityCtrl) {
+        AcuityCtrl.init(attrs);
+      }
+    };
+  }]);
