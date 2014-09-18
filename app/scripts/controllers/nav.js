39056ec7e657bbe0ed61d87c6192e9db69bb04ba
Lee Briggs <lee.briggs@headlondon.com>
2014-09-18 11:51:45 +0100

    +	added workflow sites to nav controller to enable links to each site to be displayed

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -8,8 +8,26 @@
  * Controller of the openeyesApp
  */
 angular.module('openeyesApp')
-  .controller('NavCtrl', ['$scope', '$location', function ($scope, $location) {
-    $scope.isActive = function (location) {
-      return location === $location.path();
+  .controller('NavCtrl', ['$scope', '$location', 'Event', function ($scope, $location, Event) {
+
+    var currentSite = Event.getCurrentSite();
+    if(currentSite === null){
+      Event.setCurrentSite(0);
+    }
+
+    Event.getWorkflowConfig()
+      .success(function(data){
+        $scope.sites = data;
+      })
+      .error(function(data, status, headers, config) {
+        console.log(data, status, headers, config);
+      });
+
+    $scope.isActive = function (index) {
+      return index === Event.getCurrentSite();
+    };
+
+    $scope.changeSite = function(index){
+      Event.setCurrentSite(index);
     };
   }]);
