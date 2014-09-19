f0e28460daade9118f298d402eac82a828761d77
Richard Willis <willis.rh@gmail.com>
2014-09-19 11:11:27 +0100

    Reverted a9285661043decb9999a37596879a3d9c9aa1898

--- app/scripts/directives/validate.js
+++ app/scripts/directives/validate.js
@@ -41,30 +41,41 @@
     // determining the validity of such models.
     .controller('oeValidateCtrl', [ '$scope','$element','$attrs','oeValidateInvalidMessages', function($scope, $element, $attrs, oeValidateInvalidMessages) {
 
-      var ngModels = [];
+      var element = null;
+      var model = null;
       var rules = {};
+      var err = {};
 
       this.init = function(_rules) {
         rules = _rules;
       };
 
-      this.registerModel = function(ngModel) {
-        ngModels.push(ngModel);
-      };
+      this.registerModel = function(ngModel, attrs, _element) {
 
-      this.deregisterModel = function(ngModel) {
-        var index = this.ngModels.indexOf(ngModel);
-        if (index !== -1) {
-          this.ngModels.splice(index, 1);
-        }
+        element = _element;
+        model = ngModel;
+
+        // We do this to force the validated related watches to update on blur.
+        element.on('blur', function() {
+          $scope.$apply(function() {
+            model.$setViewValue(model.$viewValue);
+          });
+        });
       };
 
       this.hasError = function(rule) {
-        var invalidModels = ngModels.filter(function(model) {
-          var isDirty = ($scope.form.submitted || model.$dirty);
-          return isDirty && (!rule ? model.$invalid : model.$error[rule]);
-        });
-        return ngModels.length && (invalidModels.length === ngModels.length);
+
+        var ruleName = rule || 'none';
+
+        // If focused, return the previous validation state.
+        if (element.is(':focus')) {
+          return err[ruleName];
+        }
+
+        var isDirty = ($scope.form.submitted || model.$dirty);
+        err[ruleName] = isDirty && (!rule ? model.$invalid : model.$error[rule]);
+
+        return err[ruleName];
       };
 
       this.getErrorMessage = function(rule) {
@@ -90,16 +101,16 @@
     // Angular's form validation doesn't work with dynamically named fields. So we need to
     // add the name attribute before other directives are compiled.
     .directive('oeName', ['$compile','$interpolate', function ($compile, $interpolate) {
-        return {
-            priority: 9999,
-            scope: false,
-            controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
-                var interpolatedName = $interpolate($attrs.oeName)($scope);
-                if (interpolatedName) {
-                  $attrs.$set('name', interpolatedName);
-                }
-            }]
-        };
+      return {
+        priority: 9999,
+        scope: false,
+        controller: ['$scope', '$element', '$attrs', function ($scope, $element, $attrs) {
+          var interpolatedName = $interpolate($attrs.oeName)($scope);
+          if (interpolatedName) {
+            $attrs.$set('name', interpolatedName);
+          }
+        }]
+      };
     }])
     // Shows contextual validation messages.
     .directive('oeValidateMsg', [function() {
@@ -112,91 +123,6 @@
         templateUrl: 'views/directives/validate-msg.html'
       };
     }])
-    .controller('oeValidateFormErrorsCtrl', function($scope, $attrs, $parse, oeValidateInvalidMessages) {
-
-      var rules = {};
-
-      this.init = function() {
-        rules = $parse($attrs.rules)($scope);
-      };
-
-      this.formatErrors = function(errors) {
-
-        var formattedErrors = [];
-        var names = [];
-
-        Object.keys(errors).forEach(function(key) {
-          var models = errors[key];
-          if (!angular.isArray(models)) {
-            return;
-          }
-          models.forEach(function(model) {
-            if (names.indexOf(model.$name) === -1) {
-              names.push(model.$name);
-              formattedErrors.push(model);
-            }
-          });
-        });
-        return formattedErrors;
-      };
-
-      this.formErrors = function() {
-        return this.formatErrors($scope.form.submitted ? $scope.form.$error : {});
-      };
-
-      this.hasFormErrors = function() {
-        return ($scope.form.submitted && $scope.form.$invalid);
-      };
-
-      this.getErrorMessage = function(name, rule) {
-        if (rule && rules[name] && rules[name][rule] && rules[name][rule].msg) {
-          return rules[name][rule].msg;
-        }
-        return oeValidateInvalidMessages[rule] || 'This field has an error.';
-      };
-
-      this.scroll = function($event, model) {
-
-        $event.preventDefault();
-
-        var element = angular.element('[name="' + model.$name + '"]');
-
-        if (!element.length) {
-          console.warn('Attempting to scroll to an element that does not exist.');
-          return;
-        }
-
-        var parentNode = element[0];
-        while((parentNode = parentNode.parentNode)) {
-          if (angular.element(parentNode).hasClass('form-group')) {
-            break;
-          }
-        }
-        if (parentNode) {
-          parentNode.scrollIntoView(true);
-        } else {
-          console.warn('Unable to find form-group wrapper for this field.');
-        }
-      };
-
-      $scope.formErrors = this.formErrors.bind(this);
-      $scope.hasFormErrors = this.hasFormErrors.bind(this);
-      $scope.getErrorMessage = this.getErrorMessage.bind(this);
-      $scope.scroll = this.scroll.bind(this);
-    })
-    // Show form validation errors.
-    .directive('oeValidateFormErrors', function() {
-      return {
-        restrict: 'E',
-        replace: false,
-        scope: true,
-        templateUrl: 'views/directives/validate-form-errors.html',
-        controller: 'oeValidateFormErrorsCtrl',
-        link: function(scope, element, attr, oeValidateFormErrorsCtrl) {
-          oeValidateFormErrorsCtrl.init();
-        }
-      };
-    })
     // Accepts rules for specific fields and recompiles the field elements with
     // the correct validation attribute directives.
     .controller('oeValidateRulesCtrl', function($scope, $element, $compile) {
@@ -247,7 +173,7 @@
       return {
         restrict: 'A',
         replace: false,
-        require: ['^?oeValidate', '^oeValidateRules'],
+        require: ['^oeValidate', '^oeValidateRules'],
         controller: 'oeValidateRulesCtrl',
         scope: true,
         terminal: true,
@@ -255,7 +181,6 @@
         link: function(scope, element, attrs, controllers) {
 
           var oeValidateCtrl = controllers[0];
-
           var oeValidateRulesCtrl = controllers[1];
           var rules = $parse(attrs.oeValidateRules)(scope);
 
@@ -280,7 +205,7 @@
           var oeValidateCtrl = controllers[0];
           var ngModel = controllers[1];
           if (oeValidateCtrl && ngModel && ngModel.$name) {
-            oeValidateCtrl.registerModel(ngModel, attrs);
+            oeValidateCtrl.registerModel(ngModel, attrs, element);
           }
         }
       };
@@ -298,7 +223,7 @@
           var oeValidateCtrl = controllers[0];
           var ngModel = controllers[1];
           if (oeValidateCtrl && ngModel && ngModel.$name) {
-            oeValidateCtrl.registerModel(ngModel, attrs);
+            oeValidateCtrl.registerModel(ngModel, attrs, element);
           }
         }
       };
