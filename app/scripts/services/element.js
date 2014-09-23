4f0a813f6d16423c28eaaeb9cb5f27ace8f910d2
Lee Briggs <lee.briggs@headlondon.com>
2014-09-23 15:41:44 +0100

    +	updated element service to use real api call

--- app/scripts/services/element.js
+++ app/scripts/services/element.js
@@ -2,48 +2,21 @@
 
 /**
  * @ngdoc function
- * @name openeyesApp.service:Event
+ * @name openeyesApp.service:Element
  * @description
- * # Event
+ * # Element
  * Service of the openeyesApp
  */
 angular.module('openeyesApp')
-  .factory('Element', ['$http', '$q', 'ENV', 'MODEL_DOMAIN', function($http, $q, ENV, MODEL_DOMAIN) {
-
-    console.log(ENV);
+  .factory('Element', ['$http', '$q', 'ENV', function($http, $q, ENV) {
 
     return {
       getElements: function(patientId, eType, date){
-        console.log(patientId, eType, date);
-        // var apiCall = ENV.host + ENV.apiEndpoints.getElement.replace(':id', patientId);
-        // return $http({
-        //   method: 'GET',
-        //   url: apiCall
-        // });
-        var data = {};
-        data[MODEL_DOMAIN + 'VisualAcuity'] = [
-          {
-            type: '',
-            rightEye: {
-              readings: [
-                { value: 1.0, correction: 'Glasses' }
-              ],
-              comment: ''
-            },
-            leftEye: {
-              readings: [
-                { value: 2.0, correction: 'Contact lenses' }
-              ],
-              comment: ''
-            }
-          }
-        ];
-
-        var deferred = $q.defer();
-        deferred.resolve(data[eType]);
-
-        return deferred.promise;
-
+        var apiCall = ENV.host + ENV.apiEndpoints.getElements.replace(':patientId', patientId).replace(':elementType', eType).replace(':date', date);
+        return $http({
+          method: 'GET',
+          url: apiCall
+        });
       }
     };
 
