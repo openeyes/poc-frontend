6ce21d5e496f86d3c848d814aef6698c2a4a83b0
Lee Briggs <lee.briggs@headlondon.com>
2014-09-25 11:10:22 +0100

    +	updated treatment order to match api post reqs

--- app/scripts/directives/treatmentOrder.js
+++ app/scripts/directives/treatmentOrder.js
@@ -18,6 +18,8 @@ angular.module('openeyesApp')
 
     this.init = function(attrs){
 
+      $scope.model = {};
+      $scope.temp = {};
       this.eyeSide = $scope.side = attrs.side;
 
       //  Listen for save event
@@ -65,6 +67,41 @@ angular.module('openeyesApp')
     };
 
     this.getModel = function(){
+
+      if($scope.temp.selectedPreAntiseptic){
+        $scope.model.preAntiseptic = $scope.temp.selectedPreAntiseptic.label;
+      }
+
+      if($scope.temp.selectedPreSkinCleanser){
+        $scope.model.preSkinCleanser = $scope.temp.selectedPreSkinCleanser.label;
+      }
+
+      if($scope.temp.selectedPreIopLoweringTherapy){
+        $scope.model.preIopLoweringTherapy = [];
+        for(var i = 0;i < $scope.temp.selectedPreIopLoweringTherapy.length;i++){
+          $scope.model.preIopLoweringTherapy.push($scope.temp.selectedPreIopLoweringTherapy[i].label);
+        }
+      }
+
+      if($scope.temp.selectedDrug){
+        $scope.model.drug = $scope.temp.selectedDrug.label;
+      }
+
+      if($scope.temp.selectedPostIopLoweringTherapy){
+        $scope.model.postIopLoweringTherapy = [];
+        for(var x = 0;x < $scope.temp.selectedPostIopLoweringTherapy.length;x++){
+          $scope.model.postIopLoweringTherapy.push($scope.temp.selectedPostIopLoweringTherapy[x].label);
+        }
+      }
+
+      if($scope.model.sequenceNumber){
+        $scope.model.sequenceNumber = parseInt($scope.model.sequenceNumber, 10);
+      }
+
+      if($scope.temp.injectionTime){
+        $scope.model.injectionTime = $scope.temp.injectionTime;
+      }
+
       return {
         name: MODEL_DOMAIN + 'TreatmentOrder',
         subPath: this.eyeSide,
