3333d715eb3461dd3cf5e0fbe6799c8ca5542b61
Richard Willis <willis.rh@gmail.com>
2014-09-25 08:05:36 +0100

    Patient list and steps now using Ticket service

--- app/scripts/controllers/patients.js
+++ app/scripts/controllers/patients.js
@@ -1,7 +1,16 @@
 'use strict';
 
 angular.module('openeyesApp')
-  .controller('PatientsCtrl', ['$scope', '$routeParams', 'Event', 'Patient', function ($scope, $routeParams, Event, Patient) {
+  .controller('PatientsCtrl', ['$scope', '$routeParams', 'Event', 'Ticket', function ($scope, $routeParams, Event, Ticket) {
+
+    function getTickets() {
+      Ticket.getTickets($scope.workflow._id.$oid, 0)
+        .then(function(data) {
+          $scope.tickets = data.data;
+        }, function(data, status, headers, config) {
+          console.log('Error getting patients', data, status, headers, config);
+        });
+    }
 
     Event.getWorkflowConfig($routeParams.workflowId)
       .then(function(data){
@@ -10,18 +19,12 @@ angular.module('openeyesApp')
         console.log('Error getting workflow', data, status, headers, config);
       });
 
-    Patient.getPatientsForStep()
-      .then(function(data) {
-        $scope.patients = data.data;
-      }, function(data, status, headers, config) {
-        console.log('Error getting patients for step:', step, data, status, headers, config);
-      });
-
     $scope.stepIndex = parseInt($routeParams.stepIndex, 10);
 
     $scope.$watch('workflow', function(workflow) {
       if (workflow !== undefined) {
         $scope.step = $scope.workflow.steps[$scope.stepIndex];
+        getTickets();
       }
     });
   }]);
---
+++ app/scripts/services/ticket.js
@@ -0,0 +1,33 @@
+'use strict';
+
+angular.module('openeyesApp')
+  .factory('Ticket', ['$http', '$q', 'ENV', function($http, $q, ENV) {
+
+    return {
+
+      getTickets: function(workflowId, stepIndex) {
+
+        var apiCall = ENV.host + ENV.apiEndpoints.getTickets
+          .replace(':workflowId', workflowId)
+          .replace(':stepIndex', stepIndex || 0);
+
+        return $http({
+          method: 'GET',
+          url: apiCall,
+          cache: true
+        });
+      },
+
+      getTicket: function(ticketId) {
+
+        var apiCall = ENV.host + ENV.apiEndpoints.getTicket
+          .replace(':ticketId', ticketId)
+
+        return $http({
+          method: 'GET',
+          url: apiCall,
+          cache: true
+        });
+      }
+    };
+  }]);
