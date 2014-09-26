05397c4411ead91c3747e475c1f2d15a7fc3e74b
Lee Briggs <lee.briggs@headlondon.com>
2014-09-26 12:35:47 +0100

    +	added new landing page controller to set nav variables

---
+++ app/scripts/controllers/landing.js
@@ -0,0 +1,29 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name openeyesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the openeyesApp
+ */
+angular.module('openeyesApp')
+  .controller('LandingCtrl', ['$scope', '$location', '$rootScope', function ($scope, $location, $rootScope) {
+
+    var self = this;
+
+
+    $rootScope.$on( '$routeChangeSuccess', function() {
+      self.setLanding();
+    });
+
+    this.setLanding = function(){
+      if($location.url() === '/'){
+        $scope.landing = true;
+      } else {
+        $scope.landing = false;
+      }
+    };
+
+    this.setLanding();
+  }]);
