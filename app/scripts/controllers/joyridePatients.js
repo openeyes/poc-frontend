f17e90feb5a7cc0aa80f58f9e9ce43ef8fd25921
mairead <mdb.buchan@gmail.com>
2014-10-08 11:45:23 +0100

    adding missing CSS back in

--- app/scripts/controllers/joyridePatients.js
+++ app/scripts/controllers/joyridePatients.js
@@ -8,18 +8,43 @@
  * Controller of the openeyesApp
  */
 angular.module('openeyesApp')
-  .controller('JoyridePatientsCtrl', ['$scope', '$rootScope', function ($scope,  $rootScope) {
+  .controller('JoyridePatientsCtrl', ['$scope', '$rootScope', '$location', function ($scope,  $rootScope, $location) {
 
-    //determine which variant on the patients list page we are on before assigning scope object
+    console.log('hey');
 
-  	$scope.config = [{      
-  		type: "element",
-      selector: "#workflow0",
+    $scope.config = [{
+      type: "title",
       heading: "Test Item 1",
-      placement: "right",
       text: "As an administrator, you can easily configure configure clinic workflows to reflect your organisation’s practices."
     }];
 
+    // var config0 = [{      
+    //   type: "title",
+    //   heading: "Test Item 1",
+    //   text: "As an administrator, you can easily configure configure clinic workflows to reflect your organisation’s practices."
+    // }];
+
+    // var config1 = [{      
+    //   type: "title",
+    //   heading: "Test Item 2",
+    //   text: "As an administrator, you can easily configure configure clinic workflows to reflect your organisation’s practices."
+    // }];
+
+
+    
+    // //determine which variant on the patients list page we are on before assigning scope object
+    // var locationPath = parseInt($location.$$path.slice(-1),10);
+
+    // //console.log($scope, $location, $rootScope, locationPath);
+
+    // switch(locationPath){
+    //   case 0: $scope.config = config0; 
+    //   break;
+    //   case 1: $scope.config = config1; 
+    //   break;
+    // }
+
+  
     $scope.startJoyRide = true;
 
   }]);
