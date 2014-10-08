723ffa94aa48c8c4d05996894859846f92f666eb
mairead <mdb.buchan@gmail.com>
2014-10-08 11:46:26 +0100

    Adding conditional joyride content in to page

--- app/scripts/controllers/joyridePatients.js
+++ app/scripts/controllers/joyridePatients.js
@@ -12,37 +12,37 @@ angular.module('openeyesApp')
 
     console.log('hey');
 
-    $scope.config = [{
-      type: "title",
-      heading: "Test Item 1",
-      text: "As an administrator, you can easily configure configure clinic workflows to reflect your organisation’s practices."
-    }];
-
-    // var config0 = [{      
+    // $scope.config = [{
     //   type: "title",
     //   heading: "Test Item 1",
     //   text: "As an administrator, you can easily configure configure clinic workflows to reflect your organisation’s practices."
     // }];
 
-    // var config1 = [{      
-    //   type: "title",
-    //   heading: "Test Item 2",
-    //   text: "As an administrator, you can easily configure configure clinic workflows to reflect your organisation’s practices."
-    // }];
+    var config0 = [{      
+      type: "title",
+      heading: "Test Item 1",
+      text: "As an administrator, you can easily configure configure clinic workflows to reflect your organisation’s practices."
+    }];
+
+    var config1 = [{      
+      type: "title",
+      heading: "Test Item 2",
+      text: "As an administrator, you can easily configure configure clinic workflows to reflect your organisation’s practices."
+    }];
 
 
     
-    // //determine which variant on the patients list page we are on before assigning scope object
-    // var locationPath = parseInt($location.$$path.slice(-1),10);
+    //determine which variant on the patients list page we are on before assigning scope object
+    var locationPath = parseInt($location.$$path.slice(-1),10);
 
-    // //console.log($scope, $location, $rootScope, locationPath);
+    //console.log($scope, $location, $rootScope, locationPath);
 
-    // switch(locationPath){
-    //   case 0: $scope.config = config0; 
-    //   break;
-    //   case 1: $scope.config = config1; 
-    //   break;
-    // }
+    switch(locationPath){
+      case 0: $scope.config = config0; 
+      break;
+      case 1: $scope.config = config1; 
+      break;
+    }
 
   
     $scope.startJoyRide = true;
