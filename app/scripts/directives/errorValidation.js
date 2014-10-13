e585cb110c20e13414cc4aca1ba499c18d676407
mairead <mdb.buchan@gmail.com>
2014-10-13 14:09:26 +0100

    Tidying look and feel of error validation

--- app/scripts/directives/errorValidation.js
+++ app/scripts/directives/errorValidation.js
@@ -13,20 +13,22 @@ angular.module('openeyesApp')
     var self = this;
 
     $scope.$on('error.invalidForm', function(){
-      self.element.addClass('show-validation');
+      self.errorMsg.addClass('show-validation');
       //when the form is invalid add the class that makes it show
       $timeout(function(){
-        self.element.addClass('fadeout-validation');
+        self.errorMsg.addClass('fadeout-validation');
       },1000); //after 1 sec start fading out
 
       //then remove the class and fade out
       $timeout(function(){
-        self.element.removeClass('show-validation');
+        self.errorMsg.removeClass('show-validation');
+        self.errorMsg.removeClass('fadeout-validation');
       },2000); //after 2 secs remove from screen
     });
 
     this.init = function(element){
       this.element = element;
+      this.errorMsg = element.find('.error-validation');
     };
 
   }])
