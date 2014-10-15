5b174152702f167980a3e1684bec6a978fc7cf7a
mairead <mdb.buchan@gmail.com>
2014-10-15 14:46:29 +0100

    trying to debug broken hg-click

--- app/scripts/directives/hiddenButton.js
+++ app/scripts/directives/hiddenButton.js
@@ -3,20 +3,23 @@
 angular.module('openeyesApp')
   .controller('HiddenButtonCtrl', ['$scope',function($scope){
 
-    this.init = function(element){
-      console.log("init?")
+    this.init = function($scope, element){
+      
       $scope.hideModal = function(){
+        console.log("clicked");
         $('.modal').modal('hide');
       }
     };
+
+
   }])
   .directive('oeHiddenButton', [function() {
     return {
       scope: {},
       restrict: 'A',
       controller: 'HiddenButtonCtrl',
-      link: function($scope, element, attr, HiddenButtonCtrl) {
-        HiddenButtonCtrl.init(element);
+      link: function(scope, element, attr, HiddenButtonCtrl) {
+        HiddenButtonCtrl.init(scope, element);
       }
     };
   }]);
